a52329abd99ddf12a060f83239bbe2cbc50c883d
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-09-30 15:12:11 -0700

    stuff

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,15 @@
-// Google fact check
-//  var key = AIzaSyAv5_cc1x2RMnjTtR8q0Iw0wd-jaMoeZj0;
-//  var URL = https://factchecktools.googleapis.com/$discovery/rest?version=v1alpha1&key=AIzaSyAv5_cc1x2RMnjTtR8q0Iw0wd-jaMoeZj0.;
-
-// 739cc82f48msh772533553684212p13fec1jsnb72c85e62170
+// var qArticle = userInput();
 
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://adverifai-api.p.rapidapi.com/source_check",
+	"url": "https://api-hoaxy.p.rapidapi.com/articles?sort_by=relevant&use_lucene_syntax=true&query="+qArticle+"%20AND%20date_published%253A%255B2016-10-28%20TO%202016-12-04%255D",
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "adverifai-api.p.rapidapi.com",
+		"x-rapidapi-host": "api-hoaxy.p.rapidapi.com",
 		"x-rapidapi-key": "739cc82f48msh772533553684212p13fec1jsnb72c85e62170"
 	}
 }
-
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
