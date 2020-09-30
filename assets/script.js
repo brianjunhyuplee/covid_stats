7b21288a5633e8433f24076ef63ecff6ddc050a4
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-09-30 14:29:30 -0700

    api keys

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
-// API Key = AIzaSyAv5_cc1x2RMnjTtR8q0Iw0wd-jaMoeZj0
+// Google fact check
+//  var key = AIzaSyAv5_cc1x2RMnjTtR8q0Iw0wd-jaMoeZj0;
+//  var URL = https://factchecktools.googleapis.com/$discovery/rest?version=v1alpha1&key=AIzaSyAv5_cc1x2RMnjTtR8q0Iw0wd-jaMoeZj0.;
+
+// 739cc82f48msh772533553684212p13fec1jsnb72c85e62170
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://adverifai-api.p.rapidapi.com/source_check",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "adverifai-api.p.rapidapi.com",
+		"x-rapidapi-key": "739cc82f48msh772533553684212p13fec1jsnb72c85e62170"
+	}
+}
+
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
