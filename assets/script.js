6b96dccf00699ae97fd72d220b6dd6e6b1f303d4
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-02 15:37:55 -0700

    url guardian

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,75 @@
-// var qArticle = userInput
-var articleId;
-var articleId1;
-var articleId2;
-var articleId3;
-var articleId4;
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://api-hoaxy.p.rapidapi.com/articles?sort_by=relevant&use_lucene_syntax=true&query=trump%20AND%20date_published%253A%255B2016-10-28%20TO%202016-12-04%255D",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-hoaxy.p.rapidapi.com",
-		"x-rapidapi-key": "739cc82f48msh772533553684212p13fec1jsnb72c85e62170"
-	}
-}
+$(document)
+  .ready(function () {
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-    articleId = response.articles[0].id;
-    articleId1 = response.articles[1].id;
-    articleId2 = response.articles[2].id;
-    articleId3 = response.articles[3].id;
-    articleId4 = response.articles[4].id;
-    console.log(articleId);
-    getTweet()
-    
-});
+    // var qArticle = userInput
+    var articleId;
+    var articleId1;
+    var articleId2;
+    var articleId3;
+    var articleId4;
 
 
-function getTweet(){
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://api-hoaxy.p.rapidapi.com/tweets?ids=%255B"+articleId+"%252C%"+articleId1+"%252C%"+articleId2+"%252C%"+articleId3+"%252C%"+articleId4+"%255D",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-hoaxy.p.rapidapi.com",
-		"x-rapidapi-key": "739cc82f48msh772533553684212p13fec1jsnb72c85e62170"
-	}
-}
 
-$.ajax(settings).done(function (response) {
-    console.log("https://api-hoaxy.p.rapidapi.com/tweets?ids=%255B"+articleId+"%252C%"+articleId1+"%252C%"+articleId2+"%252C%"+articleId3+"%252C%"+articleId4+"%255D");
-	console.log(response);
-});
-}
+
+
+    $(document).on('keypress', function (enter) {
+      if (enter.which == 13) {
+        var countryName = $("#search").val();
+        $("#loading").addClass("loading");
+        console.log(countryName);
+
+
+        var queryURL = 'https://content.guardianapis.com/search?q=%22covid%20' + countryName + '%22&api-key=1cd5cbec-afb6-4aea-b671-03563ccf7f61';
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function (response) {
+          console.log(response);
+        });
+      }
+
+      // var settings = {
+      // 	"async": true,
+      // 	"crossDomain": true,
+      // 	"url": "https://api-hoaxy.p.rapidapi.com/articles?sort_by=relevant&use_lucene_syntax=true&query=" + search,
+      // 	"method": "GET",
+      // 	"headers": {
+      // 		"x-rapidapi-host": "api-hoaxy.p.rapidapi.com",
+      // 		"x-rapidapi-key": "739cc82f48msh772533553684212p13fec1jsnb72c85e62170"
+      // 	}
+      // }
+
+      // $.ajax(settings).done(function (response) {
+      //     console.log(response);
+      //     $("#loading").removeClass("loading");
+      // });
+      // }
+      // });
+
+
+      // fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function () {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function () {
+            $('.fixed.menu').transition('fade out');
+          }
+        });
+
+      // create sidebar and attach to menu open
+      $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item')
+        ;
+
+      // $(window).scroll(function(){
+      //   $(".ui.header").fadeIn(1500);
+      // });
+      $(window).ready(function () {
+        $(".ui.text.container").fadeIn(1500);
+      });
+    });
+  });
