9cedfedf15063e75e5ef54c2577bbcb6d920266d
Aaron Diggdon <aarondiggdon@gmail.com>
2020-10-04 17:55:15 -0700

    added new api

--- script.js
+++ script.js
@@ -1,43 +1,90 @@
 $(document)
-.ready(function() {
-
-// var qArticle = userInput
-var articleId;
-var articleId1;
-var articleId2;
-var articleId3;
-var articleId4;
+  .ready(function () {
+    $(document).on('keypress', function (enter) {
+      if (enter.which == 13) {
+        var countryName = $("#search").val();
+        $("#loading").addClass("loading");
+        console.log(countryName);
 
 
 
+       
 
 
-$(document).on('keypress',function(enter) {
-  if(enter.which == 13) {
-    var search = $("#search").val();
-    $("#loading").addClass("loading");
-    console.log(search);
+      $.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=covid "+ countryName +"&sortBy=popularity&apiKey=3ae30d398e914295b27c15aeddce8ec5",
+        method: "GET"
+      }).then(function (response) {
+        
+        
+
+
+        for (var i = 0; i < 5; i++){
+              
+              $("#loading").removeClass("loading");
+              
+              var websiteUrl = response.articles[i].url;
+              var websitetitle = response.articles[i].title;
+              
+            }
+        
 
 
 
+          
+          var column = $("<div>").addClass("column");
+          var segment = $("<div>").addClass("ui segment");
+          var article = $("<iframe>").attr("src", websiteUrl);
+          $(".ui.three.column.grid").append(column.append(segment.append(article)));
 
 
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://api-hoaxy.p.rapidapi.com/articles?sort_by=relevant&use_lucene_syntax=true&query=" + search,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-hoaxy.p.rapidapi.com",
-		"x-rapidapi-key": "739cc82f48msh772533553684212p13fec1jsnb72c85e62170"
-	}
-}
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-    $("#loading").removeClass("loading");
-});
-}
+        
+      });
+
+
+        
+    //     var url = '';
+    //     var req = new Request(url);
+    //     fetch(req).then(function(response) {
+    //     console.log(response.json());
+    // })
+
+        //get related articles by country on covid
+        // var queryURL = 'https://content.guardianapis.com/search?q=%22covid%20' + countryName + '%22&api-key=1cd5cbec-afb6-4aea-b671-03563ccf7f61';
+        // $.ajax({
+        //   url: queryURL,
+        //   method: "GET"
+        // }).then(function (response) {
+        //   console.log(response.response.results);
+        //   for (var i = 0; i < response.response.results.length;i++){
+        //     if (i>3){
+        //       break;
+        //     }
+        //     var websiteUrl = response.response.results[i].webUrl;
+        //     var websitetitle = response.response.results[i].webTitle;
+        //   }
+        //   
+
+        // });
+
+      
+        //get stats covid by country
+      //   var URL = 'https://api.covid19api.com/country/'+ countryName;
+      //   $.ajax({
+      //     url: URL,
+      //     method: "GET"
+      //   }).then(function (response) {
+      //     var lastIndex = response.length-1;
+      //     var latestData = response[lastIndex];
+      //     var dateOf = response[lastIndex].Date;
+      //     var confirmedCases = response[lastIndex].Confirmed;
+      //     var activeCases = response[lastIndex].Active;
+      //     var deathNum = response[lastIndex].Deaths;
+      //     var recoveredNum = response[lastIndex].Recovered;
+      // });
+
+    }
 });
 
 
@@ -62,6 +109,6 @@ $.ajax(settings).done(function (response) {
   //   $(".ui.header").fadeIn(1500);
   // });
   $(window).ready(function() {
-    $(".ui.text.container").fadeIn(1500);
+    $(".ui.text.container").fadeIn(500);
   });
 });
